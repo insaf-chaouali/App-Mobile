c8361deb734927d725717a17080edeb518450c42
mohamed iyed kouki <mohamediyed.kouki@enicar.ucar.tn>
2025-04-02 20:53:20 +0100

    Update HomeActivity.java

--- app/src/main/java/com/example/myapplica/HomeActivity.java
+++ app/src/main/java/com/example/myapplica/HomeActivity.java
@@ -7,13 +7,14 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
+    Button button_get_started
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Button button_get_started = findViewById(R.id.button_get_started);
+        button_get_started = findViewById(R.id.button_get_started);
 
         button_get_started.setOnClickListener(v -> {Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
             startActivity(intent);
